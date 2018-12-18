ccada7726436ebdbce6724c1ad126fe067ee3a5a
Kevin Schneider <kevin-schneider@mutagene.de>
2018-12-18 12:00:23 +0100

    Add custom formatting lib - Plotly Charts can now be rendered directly from docs fsx files Add preliminary plotly post

--- docsrc/content/index.fsx
+++ docsrc/content/index.fsx
@@ -1,5 +1,14 @@
 (**
-#Homepage
-![Distances](img/testimage.png)
-coming soon
+#CSBlog
+
+![CSB](img/CSB.png)
+
+Welcome to the CSBlog.
+This is the place where members of the department of Computational Systems Biology of the TU Kaiserslautern blog about their work,
+research, and other loosly related stuff.
+
+For more information about our research and our group in general, head to our [website](https://csb.bio.uni-kl.de/).
+If you want to take a look at our projects, head over to our [github page](https://github.com/CSBiology)
+
+Blog posts are accessible on the sidebar.
 *)
