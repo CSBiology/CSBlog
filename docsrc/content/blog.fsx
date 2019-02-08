a5e43345d14702b7fd99221ea95304169897236a
Kevin Schneider <kevin-schneider@mutagene.de>
2019-02-08 17:03:39 +0100

    link plotly post

--- docsrc/content/blog.fsx
+++ docsrc/content/blog.fsx
@@ -1,6 +1,6 @@
 (** 
 #BlogPosts
 
-* February 8, 2019: [Introduction to Data visualization using FSharp.Plotly]() by [Kevin Schneider](https://github.com/kMutagene)
+* February 8, 2019: [Introduction to Data visualization using FSharp.Plotly](/WorkshopSeries_3_plotly.html) by [Kevin Schneider](https://github.com/kMutagene)
 
 *)
