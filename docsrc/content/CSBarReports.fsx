47df951a51e868e5f3c90c8a395d3738bb5cee88
Kevin Schneider <schneider.kev@outlook.de>
2021-09-29 13:36:14 +0200

    Clean up for v2

--- docsrc/content/CSBarReports.fsx
+++
@@ -1,9 +0,0 @@
-(**
-#Internal  
-
-* ##[CSBar Report 1](CoffeeReport1.html)
-
-   Report for the timespan of 2018-08-17 - 2018-09-04 
-
-
-*)
--- docsrc/content/blog.fsx
+++
@@ -1,6 +0,0 @@
-(** 
-#BlogPosts
-
-* February 8, 2019: [Introduction to Data visualization using FSharp.Plotly](./WorkshopSeries_3_plotly.html) by [Kevin Schneider](https://github.com/kMutagene)
-
-*)
